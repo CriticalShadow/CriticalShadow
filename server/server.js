72236af3b8ee1186bfbf4dc55b567a7a18d9f5e6
Steven Williams <Steven30135@gmail.com>
2015-01-22 17:26:15 -0800

    Rearranged files, created client and server folders

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app 	= express();
+var port 	= process.env.PORT || 3000;
 
     app.get('/', function(req, res){
     	res.sendfile('index.html');
     });
 
-    app.listen(3000);
+    app.listen(port, function() {
+       console.log("Listening on " + port);
+    });
