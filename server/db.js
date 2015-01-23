72773086cf16b83a74c391cf5c11bb005e1584fe
Wes <wesleytatesmith@gmail.com>
2015-01-23 14:59:47 -0800

    Adds db schema screenshots, connects to db, and begins to create getting and setting methods

--- server/db.js
+++ server/db.js
@@ -1,60 +1,64 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('shadowdb', 'root', '', {
+var db = {};
+db.sequelize = new Sequelize('shadowdb', 'root', '', {
   dialect: "mysql" // or 'sqlite', 'postgres', 'mariadb'
 });
 
 // User ID primary key is automatically created by Sequelize, no need to define this in the model
-var User = sequelize.define('User', {
+db.User = db.sequelize.define('User', {
   name: Sequelize.STRING,
   password: Sequelize.STRING,
   email: Sequelize.STRING
 });
 
 // Map ID primary key automaticaly created.  User ID foreign key will be assigned later 
-var Map = sequelize.define('Map', {
+db.Map = db.sequelize.define('Map', {
   name: Sequelize.STRING,
   guid: Sequelize.STRING
 });
 
-
-var Location = sequelize.define('Location', {
+db.Location = db.sequelize.define('Location', {
   name: Sequelize.STRING,
   latitude: Sequelize.FLOAT,
   longitude: Sequelize.FLOAT
 });
 
 // The MapLocation is the join table between the Map and Location tables
-var MapLocation = sequelize.define('MapLocation', {});
+db.MapLocation = db.sequelize.define('MapLocation', {});
 
-MapLocation.sync();
+db.MapLocation.sync();
 
 // This table will have a foreign key relationship to the MapLocation table's primary key
-var MapLocationContent = sequelize.define('MapLocationContent', {
+db.MapLocationContent = db.sequelize.define('MapLocationContent', {
   title: Sequelize.STRING,
   icon_url: Sequelize.STRING,
   text: Sequelize.BLOB
 });
 
 // A user can create many maps.  1 to many relationship
-User.hasMany(Map);
-Map.belongsTo(User);
-
+db.User.hasMany(db.Map);
+db.Map.belongsTo(db.User);
 
 // Many to many relationship between maps and locations, through the MapLocation table
-Map.belongsToMany(Location, { through: 'MapLocation' });
-Location.belongsToMany(Map, { through: 'MapLocation' });
+db.Map.belongsToMany(db.Location, { through: 'MapLocation' });
+db.Location.belongsToMany(db.Map, { through: 'MapLocation' });
 
 // A map location has one content record associated with it. 1 to 1 relationship
-MapLocation.hasOne(MapLocationContent);
-MapLocationContent.belongsTo(MapLocation);
+db.MapLocation.hasOne(db.MapLocationContent);
+db.MapLocationContent.belongsTo(db.MapLocation);
 
 
-sequelize
-  .sync({ force: true })
-  .complete(function(err) {
-     if (!!err) {
-       console.log('An error occurred while creating the table:', err)
-     } else {
-       console.log('It worked!')
-     }
-  })
+db.sequelize
+.sync()
+.complete(function(err) {
+   if (!!err) {
+     console.log('An error occurred while creating the table:', err)
+   } else {
+     console.log('It worked!')
+   }
+});
+
+
+
+
+module.exports = db;
