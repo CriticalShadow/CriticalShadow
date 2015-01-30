45a82b555e6b8d8cf6cbd37c8e322dc702ae5c9c
Neil Lokare <neil.lokare@gmail.com>
2015-01-30 12:02:48 -0800

    Merge conflicts

--- server/server.js
+++ server/server.js
@@ -87,11 +87,12 @@ app.route('/createMaps')
     res.sendFile(path.join(__dirname, '/../client/createMaps.html'));
   })
   .post(function (req, res) {
+    var guid = Guid.v4().slice(0, 5);
     var map = req.body; //map data from client
     map.UserId = req.cookies.u_id; //adds UserId property
-    map.Guid = Guid.v4().slice(0, 5);
+    map.Guid = guid;
     handlers.setMap(map);  //Adds this map to the database
-    res.redirect('/');
+    res.send(guid);
   });
 
 //Login routes
